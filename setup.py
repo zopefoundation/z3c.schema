d652c6a243382cf2eb83f902cd7e280ac7fafcd3
Jan-Jaap Driessen <janjaapdriessen+github@gmail.com>
2011-04-03 20:17:56 +0000

    Preparing release 0.7.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name = 'z3c.schema',
-    version='0.8dev',
+    version='0.7.2',
     author = 'Zope Community',
       author_email = "zope-dev@zope.org",
       description = "Additional schema fields for Zope 3",
